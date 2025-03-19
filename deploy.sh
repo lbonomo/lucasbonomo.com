388a4ef78a8299b4b881d3bc63f11d259017a1d4
Lucas Bonomo <bonomo.lucas@gmail.com>
2025-03-19 08:24:00 -0300

    Fixing style

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 npm run build
-rsync -av themes/lb-2019/dst/* vanguard.com.ar:/var/www/lucasbonomo.com/wordpress/wp-content/themes/lb19/
-rsync -av mu-plugins/* vanguard.com.ar:/var/www/lucasbonomo.com/wordpress/wp-content/mu-plugins/
+rsync -av themes/lb-2019/dst/* root@vanguard.com.ar:/var/www/lucasbonomo.com/wordpress/wp-content/themes/lb19/
+# rsync -av mu-plugins/* vanguard.com.ar:/var/www/lucasbonomo.com/wordpress/wp-content/mu-plugins/
