70a563fc54c1f5e607a6a7d449edc3926d03da70
Lucas Bonomo <bonomo.lucas@gmail.com>
2025-12-14 17:51:02 -0300

    Home

---
+++ themes/lb-2019/src/page-templates/home-page.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Template Name: Home Page
+ * Template Post Type: page
+ *
+ * Custom page template for the full width home page.
+ *
+ * @package lb19
+ */
+
+get_header();
+?>
+
+<div class="home-page-template mdl-grid mdl-grid--no-spacing">
+	<?php
+	while ( have_posts() ) :
+		the_post();
+
+		get_template_part( 'template-parts/content', 'home-page' );
+
+		if ( comments_open() || get_comments_number() ) :
+			comments_template();
+		endif;
+
+	endwhile;
+	?>
+</div>
+
+<?php
+get_footer();
